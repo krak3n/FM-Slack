a3759bcd809c3badfa03200f5d8ecf0b39367798
James Warren <mail@jamesjwarren.com>
2015-03-12 12:35:37 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+root = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+sys.path.append(os.path.join(root, 'fmslack'))
+
+import fmslack
+
+# Setup
+
+setup(
+    name='FM-Slack',
+    version=fmslack.__version__,
+    author=fmslack.__author__,
+    author_email=fmslack.__author_email__,
+    url='https://github.com/thisissoon/FM-Slack'
+    description='This application posts currently playing track to Slack',
+    packages=find_packages(
+        exclude=[
+            'tests'
+        ]),
+    include_package_data=True,
+    zip_safe=False,
+    # Dependencies
+    install_requires=[
+        'redis===2.10.3'
+    ])
